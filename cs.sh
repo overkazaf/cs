bc9f39d2cf77f5a0b730a66512e7586c4c26af24
overkazaf <jiawu.njw@alibaba-inc.com>
2019-06-07 15:59:03 +0800

    Add Costa coupon

--- cs.sh
+++ cs.sh
@@ -31,7 +31,20 @@ function fetch_starbucks_list {
 }
 
 function get_starbucks {
-	call_curl
+	curl -X GET -H "User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 12_1_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/16C101 MicroMessenger/7.0.4(0x17000428) NetType/4G Language/zh_CN" -H "Cookie: JSESSIONID=51F17E3ADDD8EB5DC7908D93A947AE0C" -H "X-Requested-With: XMLHttpRequest" -H "Host: tq.365taoquan.cn" -H "Referer: http://tq.365taoquan.cn/seller/?productId=21&agentId=88" "http://tq.365taoquan.cn/seller//commodity/21?_=1559884551901" \
+	| jq -r 'map(select(.commodityProperty=="TICKET"))|map(.name,.sellingPrice)|reduce .[] as $item (""; if $item|type == "number" then .+"  【￥"+($item|tostring)+"】####" else .+$item end)|split("####")|.[0:-1]|join("\n")' > $tmp_curl
+  
+	echo $(cat $tmp_curl) >starbucks.txt
+	
+	dialog --textbox $tmp_curl 20 50
+}
+
+function get_costa {
+	curl -X GET -H "User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 12_1_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/16C101 MicroMessenger/7.0.4(0x17000428) NetType/4G Language/zh_CN" -H "Cookie: JSESSIONID=51F17E3ADDD8EB5DC7908D93A947AE0C" -H "X-Requested-With: XMLHttpRequest" -H "Host: tq.365taoquan.cn" -H "Referer: http://tq.365taoquan.cn/seller/?productId=28&agentId=88" "http://tq.365taoquan.cn/seller//commodity/28?_=1559884551901" \
+	| jq -r 'map(select(.commodityProperty=="TICKET"))|map(.name,.sellingPrice)|reduce .[] as $item (""; if $item|type == "number" then .+"  【￥"+($item|tostring)+"】####" else .+$item end)|split("####")|.[0:-1]|join("\n")' > $tmp_curl
+  
+	echo $(cat $tmp_curl) >costa.txt
+	
 	dialog --textbox $tmp_curl 20 50
 }
 
@@ -61,7 +74,7 @@ do
 		1)
 			get_starbucks ;;
 		2)
-			foo ;;
+			get_costa ;;
 		3)
 			foo ;;
 		4)
